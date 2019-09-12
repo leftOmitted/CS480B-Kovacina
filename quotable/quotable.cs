ec42c2c82d0c61c9f56a0860e3baeb19093fe530
Thomas <tvanzeeland21@jcu.edu>
2019-09-12 12:11:05 -0400

    recent

--- quotable/quotable.cs
+++ quotable/quotable.cs
@@ -4,5 +4,17 @@ namespace quotable
 {
     public class Class1
     {
+        int input;
+        string stringInput;
+
+     static void Main(string[] args){
+          void RandomQuoteProvider(){
+             stringInput=Console.ReadLine();
+             input = Convert.ToInt64(stringInput);
+         }
+
+     }
+
+        
     }
 }
